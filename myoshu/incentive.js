e6608a2cb70932e0acdd74052da62466e5f8ffe6
Mohamed Fouad <m.fouad@innovationhero.com>
2013-02-02 21:58:53 +0200

    Adds a one step closer solution the incentive directive http://codepen.io/InnovationHero/pen/nDxsG

---
+++ myoshu/incentive.js
@@ -0,0 +1,56 @@
+var alphabet = ["Mohamed", "Ahmed", "Fouad", "lol", "ali", "would", "like", ":-)"];
+
+var width = 960;
+var height = 700;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(32,"+(height/2)+")");
+
+function update(data) {
+  var text = svg.selectAll("text")
+      .data(data, function (d) { return d });
+
+  text.attr("class","update")
+   .transition()
+    .attr("x", function(d,i) { return i*96 ; });
+
+  text.enter().append("text")
+    .attr("class","enter")
+    .attr("dy", ".35em")
+    .attr("y", -60)
+    .attr("x", function(d,i) { return i*96; })
+    .style("fill-opacity",1e-6)
+    .text(function(d) { return d; })
+   .transition()
+    .duration(750)
+    .attr("y",0)
+    .style("fill-opacity",1);
+
+  text.exit()
+    .attr("class", "exit")
+   .transition()
+    .duration(750)
+    .attr("y", 60)
+    .style("fill-opacity",1e-6)
+    .remove();
+}
+
+update(alphabet);
+
+setInterval(function () {
+  update(shuffle(alphabet)
+         .slice(0,Math.floor(Math.random()*26))
+         .sort());
+},1500);
+
+function shuffle(array) {
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random()*m--);
+    t = array[m], array[m] = array[i], array[i] = t;
+  }
+  return array;
+}
