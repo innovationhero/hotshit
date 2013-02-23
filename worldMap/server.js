a783f7f75be440ae071acca2b1f68e7aab477ab4
Mohamed Fouad <m.fouad@innovationhero.com>
2013-02-23 08:04:01 +0200

    adds minorityreport gestures for revealjs and jscats for idea viewing, along with genetic-algorithms coffeescript

---
+++ worldMap/server.js
@@ -0,0 +1,29 @@
+config = require('./config/config');
+
+var dashboard = require('./lib/dashboard');
+var tracker = require('./lib/tracker');
+var demo = require('./lib/demo');
+
+
+dashboard.listen(config.dashboard_port, config.dashboard_address);
+console.log("Dashboard listening on http://" + (config.dashboard_address || '*') + ":" + config.dashboard_port + ".");
+
+
+// Tracker should listen on the same port as the dashboard
+tracker.listen(dashboard);
+console.log("Tracker listening on http://" + (config.dashboard_address || '*') + ":" + config.dashboard_port + "/tracking_pixel.gif.");
+
+
+// If you want to have the tracking pixel listen on a different port
+// (for instance in order to password-protect your dashboard) you can
+// uncomment this
+//
+// tracker.listen(8000, "0.0.0.0");
+
+// UDP tracking
+//
+// tracker.listenUdp(8000, "0.0.0.0");
+
+if(config.demo_mode) {
+  demo.run(tracker);
+}
