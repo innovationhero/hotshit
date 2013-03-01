3964391a5b295da84248a3f9ad206643c5cd0f89
Mohamed Fouad <m.fouad@innovationhero.com>
2013-03-01 17:23:12 +0200

    adds someshit

--- ng-json-example/app.js
+++ ng-json-example/app.js
@@ -3,7 +3,15 @@ var app = angular.module('angularjs-starter', ['jsonService']);
 app.controller('MainCtrl', function($scope, JsonService) {
   JsonService.get(function(data){
    // for each on the data object to store diff ideas
-    $scope.name = data.name;
-    $scope.children = data.children;
+   // for idea in data do
+ 	
+   // $scope.name = data.name;
+   // $scope.children = data.children;
+    $scope.data = data; 	
+    angular.forEach(data.idea){
+	$scope.id = idea.id;
+	$scope.frag = idea.frag;
+	};
+
   });
 });
