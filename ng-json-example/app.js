4af1a56805c5de9231076556a7128c337ccf93dd
Mohamed Fouad <m.fouad@innovationhero.com>
2013-03-01 15:38:48 +0200

    Adds a concept of more than one idea

--- ng-json-example/app.js
+++ ng-json-example/app.js
@@ -2,6 +2,7 @@ var app = angular.module('angularjs-starter', ['jsonService']);
 
 app.controller('MainCtrl', function($scope, JsonService) {
   JsonService.get(function(data){
+   // for each on the data object to store diff ideas
     $scope.name = data.name;
     $scope.children = data.children;
   });
