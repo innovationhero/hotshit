38499c4dcfbe45c0636da5acb0538ee8f5ab59c5
Mohamed Fouad <m.fouad@innovationhero.com>
2013-03-02 17:33:57 +0200

    adds jsontest

--- ng-json-example/app.js
+++ ng-json-example/app.js
@@ -2,27 +2,36 @@ var app = angular.module('angularjs-starter', ['jsonService']);
 
 app.controller('MainCtrl', function($scope, JsonService) {
   JsonService.get(function(data){
-
+//TODO update data.json 
 // var idea = {}; //emty idea object 
-// var ideas = []; // emty array of ideas 
-	
-	 console.log(data[0].id),
-	console.log(data[0].frag)
+// var ideas = []; // emty array of ideas 	
+//	 console.log(data[0].id),
+//	console.log(data[0].frag)
 
-
-// Idea to be posted
-myIdea = {
-	"id": ideaID,
+window.oneIdeaJson = {
+	"id": "12345678",
 	"tile": "ideaTitle",
 	"registered": true,
 	"frags": {
 		"frag": {"src": "httpvideosource","type": "video"},
 		"frag": {"src": "httpimgsource", "type": "img"},
-		"frag": {"src": "audiosource","type": "audio"},
-		"frag": {"src": "txtsource","type": "txt"}
+		"frag": {"src": "httpaudiosource","type": "audio"},
+		"frag": {"src": "httptxtsource","type": "txt"}
 	} 
-}
-// myIdea.["frag"].video
+};
+
+
+
+  $scope.jsontest = window.oneIdeaJson.frags.frag.src;
+
+   console.log($scope.jsontest);
+
+
+//TODO make a multi idea json with paging functionality too
+
+// var idea = cope.resource = new myResource(window.somePreloadedJson);
+
+// src =  myIdea.["frags"].["frag"].["src"]
 
   });
 });
