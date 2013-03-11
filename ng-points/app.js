a6d7317c82948670d67e0c720ae9a650c03e2ef3
Mohamed Fouad <m.fouad@innovationhero.com>
2013-03-11 23:46:52 +0200

    ng-points awardPoints and removePoints works nice TODO ng-points directive

--- ng-points/app.js
+++ ng-points/app.js
@@ -1,20 +1,26 @@
 var app = angular.module("points", []);
 
 app.controller("PointsCtrl", function($scope,$timeout){
-  
 	$scope.points = 99; // calculated points fro server (OnlinePoints - OfflinePoints)
 	$scope.awardPoints = function(){
 		$scope.points++;
+		console.log("$scope.awardPoints");
 		awardtimeout =  $timeout($scope.awardPoints, 1000);
 	};
 
-	var awardtimeout = $timeout($scope.awardPoints,1000);
-	$scope.stop = function(){
-         $timeout.cancel(awardtimeout);
- 	}; 
-  
 	$scope.removePoints = function(){
+
 		$scope.points--;
-		mytimeout = $timeout($scope.removePoints,1000);
+		console.log("$scope.removePoints");
+		removetimeout = $timeout($scope.removePoints,1000);
 	};
+
+// 	var awardtimeout = $timeout($scope.awardPoints, 1000); 
+//	var removetimeout = $timeout($scope.removePoints, 1000); // should only be used if i want to use the stop functionality which i dont need again!
+/*
+	$scope.stop = function(){
+	if ($timeout.cancel(removetimeout)){console.log("cancelled removetimeout")} else if 
+		($timeout.cancel(awardtimeout)){ console.log("cancelled awardtimeout")} else { console.log("nothing stopped")}
+ 	}; */
+
 });
