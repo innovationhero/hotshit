c0984a608d215b45a15518a0966bec02cb79eeb4
Mohamed Fouad <m.fouad@innovationhero.com>
2013-03-12 00:44:02 +0200

    augmented "Your Score"

--- ng-points/app.js
+++ ng-points/app.js
@@ -1,11 +1,12 @@
 var app = angular.module("points", []);
 
 app.controller("PointsCtrl", function($scope,$timeout){
+	// note that award winning must be served from the server before updated to the UI to prevent expolits via js from the client side
 	$scope.points = 99; // calculated points fro server (OnlinePoints - OfflinePoints)
 	$scope.awardPoints = function(){
 		$scope.points++;
 		console.log("$scope.awardPoints");
-		awardtimeout =  $timeout($scope.awardPoints, 1000);
+		awardtimeout =  $timeout($scope.awardPoints, 100);
 	};
 
 	$scope.removePoints = function(){
@@ -13,6 +14,10 @@ app.controller("PointsCtrl", function($scope,$timeout){
 		$scope.points--;
 		console.log("$scope.removePoints");
 		removetimeout = $timeout($scope.removePoints,1000);
+	};
+
+	$scope.awardfewPoints = function(){
+		$scope.points += 100000000000000;
 	};
 
 // 	var awardtimeout = $timeout($scope.awardPoints, 1000); 
